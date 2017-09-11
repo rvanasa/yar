27db9480d75c9aa7ba7c3143e3121421f0b38003
Ryan Vandersmith <ryanvandersmith@gmail.com>
2017-09-11 17:54:21 +0000

    Rename sorcerer.config.js to sorc.config.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var argv = require('yargs').argv;
 
 global.Promise = require('bluebird').Promise;
 
-var config = require('./sorcerer.config');
+var config = require('./sorc.config');
 config.env = argv._[0];
 
 require('sorc')(config, 'Server');
